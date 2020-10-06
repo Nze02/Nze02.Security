98031c04a0a5d435a7948673e59e654973e8a3b4
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-06 20:05:16 +0100

    feat: add ApplicationDbContext

---
+++ Nze02.Security/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nze02.Security.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nze02.Security
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Company { get; set; }
+    }   
+}
+

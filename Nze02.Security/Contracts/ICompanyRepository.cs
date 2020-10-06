249e46238b3cd26033c361f201e998a6b796acea
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-06 21:49:51 +0100

    feat: add ICompanyRepository and CompanyRepository

---
+++ Nze02.Security/Contracts/ICompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using Nze02.Security.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nze02.Security.Contracts
+{
+    interface ICompanyRepository
+    {
+        Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges);
+        Task<Company> GetCompanyAsync(int CompanyID, bool trackchanges);
+        void CreateCompany(Company company);
+        void DeleteCompany(Company company);
+    }
+}
---
+++ Nze02.Security/Repositories/CompanyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nze02.Security.Contracts;
+using Nze02.Security.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nze02.Security.Repositories
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(ApplicationDbContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .OrderBy(e => e.Id)
+            .ToListAsync();
+
+
+        public async Task<Company> GetCompanyAsync(int LGAOID, bool trackChanges) =>
+            await FindByCondition(e => e.Id.Equals(LGAOID), trackChanges)
+            .SingleOrDefaultAsync();
+
+
+        public void CreateCompany(Company company) => Create(company);
+
+        public void DeleteCompany(Company company)
+        {
+            Delete(company);
+        }
+    }
+}

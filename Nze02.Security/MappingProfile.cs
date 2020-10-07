ff9a17555586006932b616e87f8afe558a3dcbe9
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-07 01:49:27 +0100

    feat: add MappingProfile

---
+++ Nze02.Security/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Nze02.Security.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nze02.Security
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserForRegistrationDto, User>();
+        }
+        
+    }
+}

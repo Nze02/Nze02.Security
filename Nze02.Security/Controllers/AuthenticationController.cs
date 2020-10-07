dd57474ad478563ae0ee563ae2bcca43fb5f29a3
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-07 01:31:26 +0100

    feat: add AuthenticationController

---
+++ Nze02.Security/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nze02.Security.Models;
+
+namespace Nze02.Security.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        public AuthenticationController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}

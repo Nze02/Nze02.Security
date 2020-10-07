c19a4e1ba001e4915064776ac3fb1ef5c92ecb72
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-07 01:34:23 +0100

    feat: add UserForRegistrationDto

---
+++ Nze02.Security/Models/UserForRegistrationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nze02.Security.Models
+{
+    public class UserForRegistrationDto
+    {
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public ICollection<string> Roles { get; set; }
+    }
+}
